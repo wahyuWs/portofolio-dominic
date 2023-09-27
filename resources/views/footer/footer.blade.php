e119d11bf9336d58d270740f72ab8e92a5543eb7
wahyuWs <wahyusantozo36@gmail.com>
2023-09-27 19:20:27 +0700

    Slicing

---
+++ resources/views/footer/footer.blade.php
@@ -0,0 +1,17 @@
+<div class="copyrights">
+    <div class="container">
+        <div class="footer-distributed">
+            <div class="footer-left">
+                <p class="footer-links">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                </p>
+                <p class="footer-company-name">All Rights Reserved. &copy; 2018 <a href="#">Dominic</a> Design By
+                    :
+                    <a href="https://html.design/">html design</a>
+                </p>
+            </div>
+        </div>
+    </div><!-- end container -->
+</div><!-- end copyrights -->
