e7834ea4f3acfd2f9b7e0a8f854ecb8acae3ff7e
wahyuWs <wahyusantozo36@gmail.com>
2023-09-29 20:38:37 +0700

    delete unused files

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -85,10 +85,248 @@
         </path>
     </svg>
 
-    @include('about.about')
-    @include('services.services')
-    @include('portofolio.portofolio')
-    @include('contact.contact')
+    <div id="about" class="section wb">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="message-box">
+                        <h2>About Dominic.</h2>
+                        <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus
+                            bibendum. Sed fermentum est vitae rhoncus molestie. Cum sociis natoque penatibus et magnis
+                            dis
+                            parturient montes, nascetur ridiculus mus. Sed vitae rutrum neque. Ut id erat sit amet
+                            libero
+                            bibendum aliquam. Donec ac egestas libero, eu bibendum risus. Phasellus et congue justo.
+                        </p>
+                        <p>Sed vitae rutrum neque. Ut id erat sit amet libero bibendum aliquam. Donec ac egestas libero,
+                            eu
+                            bibendum risus. Phasellus et congue justo.</p>
+
+                        <a href="#" class="sim-btn btn-hover-new" data-text="Download CV"><span>Download
+                                CV</span></a>
+                    </div><!-- end messagebox -->
+                </div><!-- end col -->
+
+                <div class="col-md-6">
+                    <div class="right-box-pro wow fadeIn">
+                        <img src="uploads/about_04.jpg" alt="" class="img-fluid img-rounded">
+                    </div><!-- end media -->
+                </div><!-- end col -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end section -->
+
+    <div id="services" class="section lb">
+        <div class="container">
+            <div class="section-title text-left">
+                <h3>Services</h3>
+                <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus.
+                </p>
+            </div><!-- end title -->
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="services-inner-box">
+                        <div class="ser-icon">
+                            <i class="flaticon-seo"></i>
+                        </div>
+                        <h2>Web Development</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-4">
+                    <div class="services-inner-box">
+                        <div class="ser-icon">
+                            <i class="flaticon-development"></i>
+                        </div>
+                        <h2>Responsive Design</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-4">
+                    <div class="services-inner-box">
+                        <div class="ser-icon">
+                            <i class="flaticon-process"></i>
+                        </div>
+                        <h2>Creative Design</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-4">
+                    <div class="services-inner-box">
+                        <div class="ser-icon">
+                            <i class="flaticon-discuss-issue"></i>
+                        </div>
+                        <h2>Support</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-4">
+                    <div class="services-inner-box">
+                        <div class="ser-icon">
+                            <i class="flaticon-idea"></i>
+                        </div>
+                        <h2>Web Idea</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div><!-- end col -->
+                <div class="col-md-4">
+                    <div class="services-inner-box">
+                        <div class="ser-icon">
+                            <i class="flaticon-idea-1"></i>
+                        </div>
+                        <h2>Graphic Design</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div><!-- end col -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end section -->
+
+    <div id="portfolio" class="section lb">
+        <div class="container">
+            <div class="section-title text-left">
+                <h3>Portfolio</h3>
+                <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus.
+                </p>
+            </div><!-- end title -->
+
+            <div class="gallery-menu row">
+                <div class="col-md-12">
+                    <div class="button-group filter-button-group text-left">
+                        <button class="active" data-filter="*">All</button>
+                        <button data-filter=".gal_a">Web Development</button>
+                        <button data-filter=".gal_b">Creative Design</button>
+                        <button data-filter=".gal_c">Graphic Design</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="gallery-list row">
+                <div class="col-md-4 col-sm-6 gallery-grid gal_a gal_b">
+                    <div class="gallery-single fix">
+                        <img src="uploads/gallery_img-01.jpg" class="img-fluid" alt="Image">
+                        <div class="img-overlay">
+                            <a href="uploads/gallery_img-01.jpg" data-rel="prettyPhoto[gal]"
+                                class="hoverbutton global-radius"><i class="fa fa-picture-o"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 gallery-grid gal_c gal_b">
+                    <div class="gallery-single fix">
+                        <img src="uploads/gallery_img-02.jpg" class="img-fluid" alt="Image">
+                        <div class="img-overlay">
+                            <a href="uploads/gallery_img-02.jpg" data-rel="prettyPhoto[gal]"
+                                class="hoverbutton global-radius"><i class="fa fa-picture-o"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 gallery-grid gal_a gal_c">
+                    <div class="gallery-single fix">
+                        <img src="uploads/gallery_img-03.jpg" class="img-fluid" alt="Image">
+                        <div class="img-overlay">
+                            <a href="uploads/gallery_img-03.jpg" data-rel="prettyPhoto[gal]"
+                                class="hoverbutton global-radius"><i class="fa fa-picture-o"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 gallery-grid gal_b gal_a">
+                    <div class="gallery-single fix">
+                        <img src="uploads/gallery_img-04.jpg" class="img-fluid" alt="Image">
+                        <div class="img-overlay">
+                            <a href="uploads/gallery_img-04.jpg" data-rel="prettyPhoto[gal]"
+                                class="hoverbutton global-radius"><i class="fa fa-picture-o"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 gallery-grid gal_a gal_c">
+                    <div class="gallery-single fix">
+                        <img src="uploads/gallery_img-05.jpg" class="img-fluid" alt="Image">
+                        <div class="img-overlay">
+                            <a href="uploads/gallery_img-05.jpg" data-rel="prettyPhoto[gal]"
+                                class="hoverbutton global-radius"><i class="fa fa-picture-o"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 gallery-grid gal_c gal_a">
+                    <div class="gallery-single fix">
+                        <img src="uploads/gallery_img-06.jpg" class="img-fluid" alt="Image">
+                        <div class="img-overlay">
+                            <a href="uploads/gallery_img-06.jpg" data-rel="prettyPhoto[gal]"
+                                class="hoverbutton global-radius"><i class="fa fa-picture-o"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div id="contact" class="section db">
+        <div class="container">
+            <div class="section-title text-left">
+                <h3>Contact</h3>
+                <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus.
+                </p>
+            </div><!-- end title -->
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="contact_form">
+                        <div id="message"></div>
+                        <form id="contactForm" name="sentMessage" novalidate="novalidate">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input class="form-control" id="name" type="text"
+                                            placeholder="Your Name" required="required"
+                                            data-validation-required-message="Please enter your name.">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="form-control" id="email" type="email"
+                                            placeholder="Your Email" required="required"
+                                            data-validation-required-message="Please enter your email address.">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="form-control" id="phone" type="tel"
+                                            placeholder="Your Phone" required="required"
+                                            data-validation-required-message="Please enter your phone number.">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <textarea class="form-control" id="message" placeholder="Your Message" required="required"
+                                            data-validation-required-message="Please enter a message."></textarea>
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                </div>
+                                <div class="clearfix"></div>
+                                <div class="col-lg-12 text-center">
+                                    <div id="success"></div>
+                                    <button id="sendMessageButton" class="sim-btn btn-hover-new"
+                                        data-text="Send Message" type="submit">Send Message</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div><!-- end col -->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div><!-- end section -->
     @include('footer.footer')
 
     <a href="#" id="scroll-to-top" class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
